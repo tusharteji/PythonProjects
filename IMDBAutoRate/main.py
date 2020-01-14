ba0c0992ab0dd7d7637d72c58a52e8597ded609a
tusharteji <tushar.teji@gmail.com>
2020-01-14 15:27:13 -0600

    Adding IMDBAutoRate project

---
+++ IMDBAutoRate/main.py
@@ -0,0 +1,42 @@
+import random
+from selenium import webdriver
+import time
+
+url = "https://www.imdb.com/registration/signin?u=/"
+movie_name = "#Movie-name-goes-here"
+count = 0
+
+with open('names.txt', 'r') as f:
+    for name in f:
+        name = name.strip()
+        print("Processing " + str(count+1) + ': ' + name)
+        driver = webdriver.Chrome(r'chromedriver.exe')
+        driver.get(url)
+        driver.find_element_by_link_text('Create a New Account').click()
+        time.sleep(2)
+        driver.find_element_by_id('ap_customer_name').send_keys(name)
+        time.sleep(1)
+        driver.find_element_by_id('ap_email').send_keys(name + '_' + str(random.randrange(10000, 99999)) + '@gmail.com')
+        time.sleep(1)
+        driver.find_element_by_id('ap_password').send_keys('test' + name + '123!')
+        time.sleep(1)
+        driver.find_element_by_id('ap_password_check').send_keys('test' + name + '123!')
+        time.sleep(1)
+        driver.find_element_by_class_name('a-button-input').click()
+        time.sleep(1)
+        driver.find_element_by_id('suggestion-search').send_keys(movie_name)
+        time.sleep(1)
+        driver.find_element_by_id('suggestion-search-button').click()
+        time.sleep(2)
+        driver.find_element_by_link_text('Chhapaak').click()
+        time.sleep(2)
+        driver.find_element_by_xpath('//*[@id="star-rating-widget"]/div/button/span[1]').click()
+        time.sleep(1)
+        driver.find_element_by_xpath('//*[@id="star-rating-widget"]/div/div/span[1]/span/a[10]').click()
+        time.sleep(1)
+        driver.close()
+        count += 1
+f.close()
+driver.quit()
+print("Count: " + count)
+print("Last name processed: " + name)
